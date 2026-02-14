3bcb11aa29b97d0b08da4fbe667530ad7831580b
phyominpaing <minpaingphyo255@gmail.com>
2026-02-14 21:58:51 +0630

    integrate with axios

--- client/src/components/NoteList.tsx
+++ client/src/components/NoteList.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { createNote, getNotes } from "../services/note";
+import { createNote, deleteNote, getNotes, updateNote } from "../services/note";
 import type { Note } from "../types/note";
 
 const NoteList = () => {
   const [notes, setNotes] = useState<Note[]>([]);
   const [msg, setMsg] = useState<string>("");
   const [refresh, setRefresh] = useState<boolean>(false);
+  const [editMode, setEditMode] = useState(false);
+  const [editId, setEditId] = useState("");
 
   const makeRefresh = () => {
     setRefresh(!refresh);
-  }
+  };
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -23,11 +25,16 @@ const NoteList = () => {
     fetchNotes();
   }, [refresh]);
 
-  const addNewNote = async (e: React.FormEvent) => {
+  const submitHandler = async (e: React.FormEvent) => {
     e.preventDefault();
     if (msg.trim().length === 0) return;
     try {
-      await createNote(msg);
+      if (editMode) {
+        await updateNote(editId, msg);
+        setEditMode(false);
+      } else {
+        await createNote(msg);
+      }
       setMsg("");
       makeRefresh();
     } catch (error) {
@@ -35,15 +42,41 @@ const NoteList = () => {
     }
   };
 
+  const handleDeleteNote = async (id: string) => {
+    try {
+      await deleteNote(id);
+      makeRefresh();
+    } catch (error) {
+      throw new Error("Failed to delete note.");
+    }
+  };
+
+  const handleModeChange = (id: string, title: string) => {
+    setEditMode(true);
+    setMsg(title);
+    setEditId(id);
+  };
+
   return (
     <div>
       <h2>Note List</h2>
       <ul>
         {notes.map((note) => (
-          <li key={note._id}>{note.title}</li>
+          <li key={note._id}>
+            {note.title}{" "}
+            <button type="button" onClick={() => handleDeleteNote(note._id)}>
+              Delete{" "}
+            </button>{" "}
+            <button
+              onClick={() => handleModeChange(note._id, note.title)}
+              type="button"
+            >
+              Edit
+            </button>
+          </li>
         ))}
       </ul>
-      <form onSubmit={addNewNote} action="">
+      <form onSubmit={submitHandler} action="">
         <input
           type="text"
           name="title"
@@ -51,7 +84,7 @@ const NoteList = () => {
           value={msg}
           onChange={(e) => setMsg(e.target.value)}
         />
-        <button type="submit">Add Note</button>
+        <button type="submit">{editMode ? "Update" : "Add"}</button>
       </form>
     </div>
   );
--- client/src/services/note.ts
+++ client/src/services/note.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import type { Note } from "../types/note";
 
 let API_URL = "";
@@ -11,20 +12,59 @@ if (import.meta.env.VITE_MODE === "production") {
 
 console.log("api url", API_URL);
 
+// export const getNotes = async (): Promise<Note[]> => {
+//   const response = await fetch(`${API_URL}/todos`);
+//   const data = await response.json();
+//   return data.todos;
+// };
+
 export const getNotes = async (): Promise<Note[]> => {
-  const response = await fetch(`${API_URL}/todos`);
-  const data = await response.json();
+  const { data } = await axios.get(`${API_URL}/todos`);
   return data.todos;
 };
 
+// export const createNote = async (title: string) => {
+//   await fetch(`${API_URL}/create`, {
+//     method: "POST",
+//     body: JSON.stringify({ title }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   // const data = await response.json();
+//   // return data.todo;
+// };
+
 export const createNote = async (title: string) => {
-  await fetch(`${API_URL}/create`, {
-    method: "POST",
-    body: JSON.stringify({ title }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  // const data = await response.json();
-  // return data.todo;
+  const { data } = await axios.post(`${API_URL}/create`, { title });
+  return data.todo;
+};
+
+// export const updateNote = async (id: string, title: string) => {
+//   await fetch(`${API_URL}/todos/${id}`, {
+//     method: "PUT",
+//     body: JSON.stringify({ title }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+// };
+
+export const updateNote = async (id: string, title: string) => {
+  const { data } = await axios.put(`${API_URL}/todos/${id}`, { title });
+  return data.todo;
+};
+
+// export const deleteNote = async (id: string) => {
+//   await fetch(`${API_URL}/todos/${id}`, {
+//     method: "DELETE",
+//     body: JSON.stringify({ id }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+// };
+
+export const deleteNote = async (id: string) => {
+  await axios.delete(`${API_URL}/todos/${id}`);
 };
