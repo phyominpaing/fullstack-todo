6dc72dcc0cf1cdbb335778b8f8ebf9057c482aa6
phyominpaing <minpaingphyo255@gmail.com>
2026-03-02 20:38:38 +0630

    deploy client issue fixes 2

--- client/src/slices/api.ts
+++ client/src/slices/api.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "apiSlice",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://fullstack-todo-f476.onrender.com/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
   endpoints: () => ({}),
 });
