a58759ef04386c0700f58a6dce771bfecc150038
phyominpaing <minpaingphyo255@gmail.com>
2026-03-02 20:43:33 +0630

    deploy client issue fixes 3

--- client/src/slices/api.ts
+++ client/src/slices/api.ts
@@ -1,7 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseUrl =
+  import.meta.env.VITE_MODE === "development"
+    ? import.meta.env.VITE_LOCAL_API_URL
+    : import.meta.env.VITE_API_URL;
+
 export const apiSlice = createApi({
   reducerPath: "apiSlice",
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${baseUrl}/`,
+  }),
   endpoints: () => ({}),
 });
